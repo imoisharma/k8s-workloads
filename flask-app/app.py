269285fc3d78a76eca4e51b93cebdd25efc464a8
Mohit Sharma <imoisharma@icloud.com>
2022-01-12 20:32:06 +1000

    added the file

--- flask-app/app.py
+++ flask-app/app.py
@@ -3,4 +3,5 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Welcome, to the K8s-workshop! - By M. Sharma ('.')'
+    print("This lab is created by M. Sharma")
+    return 'Hello, Docker Lovers!'
